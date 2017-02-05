d23a0b2e0105b8d76d5d32584b16ef7aee469c3b
Florin Pop <popflorin1705@yahoo.com>
2017-02-05 18:26:59 +0200

    checking neightbors

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,9 @@ function setup() {
             grid.push(new Cell(i * gridSize, j * gridSize, gridSize));
         }
     }
+    
+    // Starting at grid[0]
+    current = grid[0];
 }
 
 function draw() {
@@ -21,7 +24,10 @@ function draw() {
     
     grid.forEach(cell => {
         cell.draw();
-    })
+    });
+    
+    current.visited = true;
+    checkNeighbors(current.x, current.y);
 }
 
 function keyPressed() {
@@ -38,3 +44,26 @@ function keyPressed() {
         
     }
 }
+
+function checkNeighbors(i, j){
+    var neighbors = [];
+    
+    var top    = grid[index(i, j-1)];
+    var right  = grid[index(i+1, j)];
+    var bottom = grid[index(i, j+1)];
+    var left   = grid[index(i-1, j)];
+    
+    if(!top.visited)
+        neighbors.push(top);
+    if(!right.visited)
+        neighbors.push(right);
+    if(!bottom.visited)
+        neighbors.push(bottom);
+    if(!left.visited)
+        neighbors.push(left);
+}
+
+function index(i, j) {
+    return i + j * gridNr;
+}
+
