b65a67f9f874ac6d83898162dda6099cdeb0b1c4
Florin Pop <popflorin1705@yahoo.com>
2017-02-05 15:08:31 +0200

    fixed express error type

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.user(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
     res.render('index.html');
