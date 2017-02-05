652f80f2593765fbbc2bd3d205891fdeb19396c4
Florin Pop <popflorin1705@yahoo.com>
2017-02-05 16:46:44 +0200

    drawing maze

--- public/js/main.js
+++ public/js/main.js
@@ -2,18 +2,18 @@ let player;
 let playerStep;
 
 let canvasSize = 800;
-let gridNr = 15;
+let gridNr = 20;
 let gridSize = playerStep = canvasSize / gridNr;
 let grids = [];
-
 
 function setup() {
     createCanvas(canvasSize, canvasSize);
     
     // Creating the grid containing the maze
     for(var i=0; i<gridNr; i++){
+        grids[i] = [];
         for(var j=0; j<gridNr; j++){
-            grids[i][j] = new Grid(i * gridSize, j * gridSize, 0);
+            grids[i][j] = new Grid(i * gridSize, j * gridSize, gridSize, 0);
         }
     }
     
@@ -22,9 +22,15 @@ function setup() {
 
 function draw() {
     background(51);
-    drawLines();
+    //drawLines();
     
-    player.draw();
+    grids.forEach(gridX => {
+        gridX.forEach(grid => {
+            grid.draw(); 
+        });
+    })
+    
+    //player.draw();
 }
 
 function keyPressed() {
